a4c76f9ad61ebd4168fb87f6fd7a5c94fc4c0fcc
adsoftsito <adsoft@live.com.mx>
2023-07-19 14:18:36 -0500

    init codebase

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import 'package:ui_one/features/auth/presentation/pages/app_widget.dart';
+import 'package:ui_one/features/auth/presentation/pages/sign_in_page.dart';
+
+import 'package:ui_one/service._locator.dart';
+
+void main() {
+  setupLocator();
+  runApp(const AppWidget());
+}
